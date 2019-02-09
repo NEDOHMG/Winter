1c10f9d0eee417dc32689e40d71b5ccd2c133bb0
Antonio Vega <a.vegaramirez76@gmail.com>
2019-02-09 19:21:49 +0900

    Mountains generator
    To do:
    
    - Change the trajectory of the user
    - Add speed to the user
    - Add jump

--- WinterGame/Assets/Scripts/Mountains/MountainsGenerator.cs
+++ WinterGame/Assets/Scripts/Mountains/MountainsGenerator.cs
@@ -5,38 +5,19 @@ using UnityEngine;
 public class MountainsGenerator : MonoBehaviour
 {
 
-    public GameObject mountainPrefab;
-    private GameObject mountain;
-
-    float yPos = 11.9f;
+    public GameObject mountainsPrefab;
+    private GameObject mountains;
 
     void Awake()
     {
-        GeneratePositionMountain();
+        GenerateMountains(-439.011f, 23.75f, 253.5f);
     }
 
-    void GeneratePositionMountain()
-    {
-        for(int i = 0; i < 3; i++)
-        {
-            if(i == 0)
-            {
-                GenerateMountain(-46.8f, yPos, -16.985f, 0, 0, 0);
-            } else if (i == 1)
-            {
-                GenerateMountain(-64.2f, yPos, 74.5f, 0, 0, 0);
-            }
-            else if (i == 2)
-            {
-                GenerateMountain(-268.9f, yPos, 59.9f, 0, 0, 0);
-            }
-        }
-    }
 
     // Update is called once per frame
-    public void GenerateMountain(float x, float y, float z, float dx, float dy, float dz)
+    void GenerateMountains(float x, float y, float z)
     {
-        mountain = Instantiate(mountainPrefab) as GameObject;
-        mountain.transform.position = new Vector3(x, y, z);
+        mountains = Instantiate(mountainsPrefab) as GameObject;
+        mountains.transform.position = new Vector3(x, y, z);
     }
 }
